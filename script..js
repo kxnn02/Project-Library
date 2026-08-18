893f7ae569288a699924b151fbd9b55baea5524b
kxnn02 <fernandezk728@gmail.com>
2026-08-18 17:15:13 +0800

    feat: add book data layer
    - add Book constructor with unique id
    
    - add addBookToLibrary function

--- script..js
+++ script..js
@@ -1,5 +1,7 @@
+// Book objects are stored here 
 const myLibrary = [];
 
+// Book Constructor 
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -8,3 +10,12 @@ function Book(title, author, pages, read) {
     this.id = crypto.randomUUID();
 }
 
+// Add Books to the library 
+function addBookToLibrary(title, author, pages, read) {
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+}
+
+addBookToLibrary("Harry Potter and the Sorcerer's Stone", "J.K Rowling", 320, true)
+
+console.log(myLibrary);
